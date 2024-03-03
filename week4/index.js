d14d84881591a37f3b9779e6ec48d05bf2417de2
witjmc <106864033+witjmc@users.noreply.github.com>
2024-03-04 00:19:48 +0900

    Create index.js

---
+++ week4/index.js
@@ -0,0 +1,12 @@
+// npm init, npm install figlet , npm 공식문서에서 figlet 예제
+var figlet = require('figlet'); //figlet 모듈을 요구하다.
+
+// Hello World 글자 출력 하는 함수 node index.js 로 console에서 확인 가능
+figlet('Hello World!!', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
