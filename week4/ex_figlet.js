ccd8505055e4d4223351ff2c9414a640f348c68f
witjmc <106864033+witjmc@users.noreply.github.com>
2024-03-04 00:24:33 +0900

    Update ex_figlet.js

--- week4/ex_figlet.js
+++ week4/ex_figlet.js
@@ -1,4 +1,5 @@
-// npm init, npm install figlet , npm 공식문서에서 figlet 예제
+// npm init, npm install figlet (-g: 전역으로 사용할 경우, 모든 드라이브에 저장될 수 있다. 프로젝트 단위대로 저장하는 것이 좋다!) 
+// npm 공식문서에서 figlet 예제
 var figlet = require('figlet'); //figlet 모듈을 요구하다.
 
 // Hello World 글자 출력 하는 함수 node ex_figlet.js 로 console에서 확인 가능
