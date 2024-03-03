30ce656ce84bfc22238004e4b7847b1e800d26c4
witjmc <106864033+witjmc@users.noreply.github.com>
2024-03-04 00:22:11 +0900

    Update and rename index.js to ex_figlet.js

--- week4/ex_figlet.js
+++ week4/ex_figlet.js
@@ -1,7 +1,7 @@
 // npm init, npm install figlet , npm 공식문서에서 figlet 예제
 var figlet = require('figlet'); //figlet 모듈을 요구하다.
 
-// Hello World 글자 출력 하는 함수 node index.js 로 console에서 확인 가능
+// Hello World 글자 출력 하는 함수 node ex_figlet.js 로 console에서 확인 가능
 figlet('Hello World!!', function (err, data) {
     if (err) {
         console.log('Something went wrong...');
