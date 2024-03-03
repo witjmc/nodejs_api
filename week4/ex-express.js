a58746e50ec364b261622d722e2093fc962a6ffa
witjmc <106864033+witjmc@users.noreply.github.com>
2024-03-04 00:41:04 +0900

    Update ex-express.js

--- week4/ex-express.js
+++ week4/ex-express.js
@@ -1,6 +1,6 @@
 // express 역할 : 웹 프레임워크(프론트엔드에서 백엔드로 요청을 보내서 백엔드에서 처리(db조회, 계산처리 등)해서 응답을 보내주는 것) 를 만들고 사용하는 것
 // 설치 : npm i express
-// 실행 : node index.js
+// 실행 : node ex-express.js
 // 웹실행 : localhost:3000
 const express = require('express');
 const app = express();
