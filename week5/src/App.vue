4701e7dd6570a257fa1bf086e57efea965ed5e41
witjmc <106864033+witjmc@users.noreply.github.com>
2024-03-13 21:21:32 +0900

    Create App.vue

---
+++ week5/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h2>Multi Checkbox</h2>
+        <MultiCheckbox :items="items" v-model="checkedNames1" />
+        <h2>Multi Checkbox2</h2>
+        <MultiCheckbox :items="items2" v-model="checkedNames2" />
+        <h2>Multi Checkbox3</h2>
+        <MultiCheckbox :items="items3" v-model="checkedNames3" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import MultiCheckbox from '@/components/MultiCheckbox.vue';
+
+const items = [
+    { id: 'jack', value: 'Jack', label: 'Jack' },
+    { id: 'john', value: 'John', label: 'John' },
+    { id: 'mike', value: 'Mike', label: 'Mike' },
+];
+
+const items2 = [
+    { id: 'jack', value: 'Jack', label: 'Jack' },
+    { id: 'john', value: 'John', label: 'John' },
+    { id: 'mike', value: 'Mike', label: 'Mike' },
+];
+
+const items3 = [
+    { id: 'jack', value: 'Jack', label: 'Jack' },
+    { id: 'john', value: 'John', label: 'John' },
+    { id: 'mike', value: 'Mike', label: 'Mike' },
+];
+
+const checkedNames1 = ref<string[]>([]);
+const checkedNames2 = ref<string[]>([]);
+const checkedNames3 = ref<string[]>([]);
+</script>
