7fe71512b00b91cb4211653dbce3ad1bf0287b7a
witjmc <106864033+witjmc@users.noreply.github.com>
2024-04-18 21:17:42 +0900

    Create App.vue
    1

---
+++ week6/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button class="btn btn-success" @click="currentTab = 'Setting1'">Setting1</button>
+        <button class="btn btn-danger" @click="currentTab = 'Setting2'">Setting2</button>
+        <component :is="currentTabComponent()"></component>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Setting1 from '@/components//Setting1.vue';
+import Setting2 from '@/components/Setting2.vue';
+
+const currentTab = ref('Setting1');
+
+const currentTabComponent = () => {
+    console.log('currentTab.value:', currentTab.value);
+    return currentTab.value === 'Setting1' ? Setting1 : Setting2;
+};
+</script>
